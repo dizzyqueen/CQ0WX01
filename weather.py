5d7e117ad9adc0e992b50ecfd17e455d2da32c69
Kevin M. Harrington MCDST <kevinh3@gmail.com>
2020-03-01 18:01:08 -0800

    updated code

--- weather.py
+++ weather.py
@@ -11,8 +11,7 @@ def index():
     celcius = round(sense.get_temperature(), 1)
     fahrenheit = round(1.8 * celcius + 32, 1)
     humidity = round(sense.get_humidity(), 1)
-# pressure = round(sense.get_pressure(), 1)
-pressure = round(33.8639 * sense.get_pressure(), 1)
+    pressure = round(sense.get_pressure(), 1)
 
     return render_template('weather.html', celcius=celcius, fahrenheit=fahrenheit, humidity=humidity, pressure=pressure)
 
